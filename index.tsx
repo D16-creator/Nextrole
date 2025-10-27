9adb88ea2a042acd563732ca781cc6ecf1b2a217
D16-creator <harperdivonte16@gmail.com>
2025-10-27 14:00:15 -0500

    Create index.tsx

---
+++ index.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontFamily: 'sans-serif'
+    }}>
+      <h1>Welcome to Nextrole!</h1>
+    </main>
+  );
+}
